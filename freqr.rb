c731c675941ab26814b4f1250710831d0221eff1
Jason Heppler <hepplerj@hotmail.com>
2011-02-14 16:11:12 -0600

    Updating last modified date

--- freqr.rb
+++ freqr.rb
@@ -8,13 +8,14 @@
 # You can distribute/modify this program under the terms of
 # the GNU Lesser General Public License version 2.1.
 # 
-# Last Modified: Mon Jan 10 23:15:08 CST 2011
+# Last Modified: Mon Feb 14 16:08:06 CST 2011
  
 require 'open-uri'
  
 STOPWORDS = %w{a about above across after again against all am an and any are arent as at be because been before being below between both but by cant cannot could couldnt did didnt do does doesnt doing dont down during each few for form further had hadnt has hasnt have havent having he her here heres hers herself him himself his how i id ill im ive if in into is isnt it its itself lets me more most mustnt my myself my myself no nor not of off on once only or other ought our ours ourselves out over own same shant she should shouldnt so some such than that the their theirs them themselves then there these they this those through to too under until up very was we were what when where which while who why with would you your yours yourself yourselves}
- 
+
 # Tag Cloud class for generating a word cloud from a word frequency
+# =================================================================
 class TagCloud
     attr_accessor :word_class
  
@@ -57,9 +58,10 @@ class TagCloud
     end
  
 end
- 
+
 # Strip out HTML tags, alphanumeric characters, and punctuation, then 
 # lower-case all words, split the words apart, and remove stopwords 
+# ===================================================================
 def readFile(url)
  
     uri_file = open(url).read.gsub(/<\/?[^>]*>/, "").gsub(/&quot;*/, "").gsub(/[0-9]*/, "").gsub(/[(,?!\'""':.)]/, '').downcase.split(' ') - STOPWORDS
@@ -67,27 +69,51 @@ def readFile(url)
     return uri_file
  
 end
- 
+
 # Create a dictionary of n-grams
+# ==============================
 url = ARGV[0]
 uri_file = readFile(url)
- 
+
 # Save output to HTML
+# ===================
 File.open("output.html", "w") do |output|
         frequency = Hash.new(0)
         uri_file.each { |word| frequency[word] += 1 }
         frequency.sort_by { |x,y| y }.reverse().each do |w,f| 
             output.write "<p>#{f}, #{w}</p>\n"
-    end
+        end
 end
- 
+
 # Generate a word cloud and save as HTML
+# ======================================
 File.open("wordcloud.html", "w") do |output|
     cloud = TagCloud.new(uri_file)
     cloud.word_class = "freq-cloud-css"
     output.write cloud.build
 end
- 
-# Give the user an exported-to message
-puts "\nFile exported to #{Dir.pwd}.\n"
 
+# Create a key words in context visualization
+# ===========================================
+=begin
+puts "Do you want to generate a keywords in context visualization? "
+kwic_answer = kwic_answer.gets.chomp
+
+if kwic_answer == "yes"
+    puts "Enter the target word: "
+    target_word = target_word.gets.chomp
+    File.open("kwic_output.html", "w") do |output|
+        kwic_viz = 
+    end
+else kwic_answer == "no"
+    return
+end
+=end
+
+# TODO:
+#   - add the remainder of the target word code
+#   - is 'return' the proper 'else' line?
+
+# Give the user an exported-to message
+# ====================================
+puts "\nFile exported to #{Dir.pwd}.\n"
